b0858b929f3a2710f3325f60386e5ee3fb8293b0
somonor011 <somonorhong011@gmail.com>
2024-12-13 15:46:42 +0700

    connect php to database MySQL

--- dbcon.php
+++ dbcon.php
@@ -1,3 +1,18 @@
 <?php 
-    
+
+    /* Database connection with mySQL */
+
+    define("HOSTNAME" , "localhost");
+    define("USERNAME" , "root");
+    define("PASSWORD" , "Smn@31012006");
+    define("DATABASE" , "db_crud_beginner");
+
+    $connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+    if(!$connection){
+        die("connection failed : " . mysqli_connect_error());
+    } else {
+        echo "connect sucessful";
+    }
+
 ?>

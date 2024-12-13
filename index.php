e40843080f7b8d18adaf87f5889a9987d9267129
somonor011 <somonorhong011@gmail.com>
2024-12-13 16:11:31 +0700

    create and include header and footer

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<!-- include header -->
 
-</head>
-
-<body>
-    <h1 id="main_title">CRUD Aplication in PHP</h1>
-    <div class="container">
-        <h2>ALL STUDENTS</h2>
-        <table class="table table-hover table-bordered table-striped">
+    <?php include('header.php') ?>
+    <h2>ALL STUDENTS</h2>
+    <table class="table table-hover table-bordered table-striped">
             <thead>
                 <tr>
                     <th>ID</th>
@@ -39,14 +27,7 @@
                 </tr>
             </tbody>
     </table>
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-        crossorigin="anonymous"></script>
-</body>
 
-</html>
+    <!-- include footer -->
+    <?php include('footer.php') ?>
+    
